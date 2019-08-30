ece5e815e38d2776bcd6638e9ecd1cfb89e727e7
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-08-30 16:31:49 -0300

    Initial commit for first project

---
+++ lista1/data/get_data.sh
@@ -0,0 +1,7 @@
+wget http://www-personal.umich.edu/~mejn/netdata/lesmis.zip
+wget http://nrvis.com/download/data/eco/eco-foodweb-baywet.zip
+wget http://www-personal.umich.edu/~mejn/netdata/netscience.zip
+wget http://snap.stanford.edu/data/facebook_combined.txt.gz
+wget http://konect.cc/files/download.tsv.subelj_euroroad.tar.bz2
+wget http://konect.uni-koblenz.de/downloads/tsv/petster-friendships-hamster.tar.bz2
+wget http://konect.uni-koblenz.de/downloads/tsv/ca-AstroPh.tar.bz2
