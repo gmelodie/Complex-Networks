5bef83ffe4294397d46b96705b35c60ee36bdb56
Gabriel Cruz <gabs.oficial98@gmail.com>
2019-10-22 12:18:41 -0300

    Add ex1 and ex2 of lista2

---
+++ lista2/data/get_data.sh
@@ -0,0 +1,16 @@
+wget http://www-personal.umich.edu/~mejn/netdata/lesmis.zip
+wget http://nrvis.com/download/data/eco/eco-foodweb-baywet.zip
+wget http://www-personal.umich.edu/~mejn/netdata/netscience.zip
+wget http://snap.stanford.edu/data/facebook_combined.txt.gz
+
+# E-road (euro road)
+wget http://konect.cc/files/download.tsv.subelj_euroroad.tar.bz2
+
+# Hamster Friendships
+wget http://konect.uni-koblenz.de/downloads/tsv/petster-friendships-hamster.tar.bz2
+
+# AstroPhisics
+wget http://konect.uni-koblenz.de/downloads/tsv/ca-AstroPh.tar.bz2
+
+# C elegans neural network
+wget http://www-personal.umich.edu/~mejn/netdata/celegansneural.zip
